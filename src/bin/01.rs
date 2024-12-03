87fe4266291a65baa59780b003c088e31a231eae
jrmoynihan <jrmoynihan@gmail.com>
2024-12-03 14:49:27 -0500

    day3-part2-parsing

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -53,6 +53,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        dbg!(&result);
         assert!(result.is_some());
     }
 
--- src/bin/03.rs
+++ src/bin/03.rs
@@ -2,32 +2,26 @@ advent_of_code::solution!(3);
 
 use miette::miette;
 use nom::{
+    branch::alt,
     bytes::complete::tag,
     character::complete::{anychar, u32},
+    combinator::value,
     multi::{many1, many_till},
     sequence::{delimited, separated_pair},
     IResult, Parser,
 };
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let (_, instructions) = parse(input)
-        .map_err(|e| miette!("parse failed! {}", e))
-        .ok()?;
-    dbg!(&instructions);
-    let sum = instructions
-        .iter()
-        .map(|i| match i {
-            Instruction::Mul(a, b) => a * b,
-            Instruction::Add(a, b) => a + b,
-        })
-        .sum();
-    Some(sum)
-}
-
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Instruction {
     Mul(u32, u32),
     Add(u32, u32),
+    Do,
+    Dont,
+}
+#[derive(PartialEq, Eq)]
+enum ShouldProcess {
+    Do,
+    Dont,
 }
 
 fn parse_mul_instruction(input: &str) -> IResult<&str, Instruction> {
@@ -36,14 +30,57 @@ fn parse_mul_instruction(input: &str) -> IResult<&str, Instruction> {
     Ok((input, Instruction::Mul(pair.0, pair.1)))
 }
 
+fn parser_rules(input: &str) -> IResult<&str, Instruction> {
+    alt((
+        value(Instruction::Dont, tag("don't()")),
+        value(Instruction::Do, tag("do()")),
+        parse_mul_instruction,
+    ))(input)
+}
+
 fn parse(input: &str) -> IResult<&str, Vec<Instruction>> {
-    many1(many_till(anychar, parse_mul_instruction).map(|(_discard, instructions)| instructions))(
-        input,
-    )
+    many1(many_till(anychar, parser_rules).map(|(_discard, instructions)| instructions))(input)
+}
+
+fn execute(instructions: &[Instruction]) -> Option<u32> {
+    let (_, sum) = instructions.iter().fold(
+        (ShouldProcess::Do, 0),
+        |(should_process, sum), instruction| match instruction {
+            Instruction::Mul(a, b) => {
+                if should_process == ShouldProcess::Do {
+                    (should_process, sum + a * b)
+                } else {
+                    (should_process, sum)
+                }
+            }
+            Instruction::Add(a, b) => {
+                if should_process == ShouldProcess::Do {
+                    (should_process, sum + a + b)
+                } else {
+                    (should_process, sum)
+                }
+            }
+            Instruction::Do => (ShouldProcess::Do, sum),
+            Instruction::Dont => (ShouldProcess::Dont, sum),
+        },
+    );
+    Some(sum)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (_, instructions) = parse(input)
+        .map_err(|e| miette!("parse failed! {}", e))
+        .ok()?;
+    dbg!(&instructions);
+    execute(&instructions)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (_, instructions) = parse(input)
+        .map_err(|e| miette!("parse failed! {}", e))
+        .ok()?;
+    dbg!(&instructions);
+    execute(&instructions)
 }
 
 #[cfg(test)]
@@ -56,8 +93,6 @@ mod tests {
             "examples", DAY, 1,
         ));
         assert_eq!(example, Some(161));
-        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
-        // assert_eq!(result, None);
     }
 
     #[test]
