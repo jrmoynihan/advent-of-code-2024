1e581f51d29e63d7b0ba419493209a734995dbc1
jrmoynihan <jrmoynihan@gmail.com>
2024-12-04 22:05:15 -0500

    day4-part2-solved

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,5 +1,9 @@
+use itertools::Itertools;
+use std::fmt::Display;
+
 advent_of_code::solution!(4);
 
+#[derive(Debug, PartialEq)]
 enum Direction {
     NorthWest,
     North,
@@ -22,6 +26,23 @@ impl Direction {
             Direction::SouthWest => (-1, 1),
             Direction::West => (-1, 0),
         }
+    }
+    fn opposite(&self) -> Direction {
+        match self {
+            Direction::NorthWest => Direction::SouthEast,
+            Direction::North => Direction::South,
+            Direction::NorthEast => Direction::SouthWest,
+            Direction::East => Direction::West,
+            Direction::SouthEast => Direction::NorthWest,
+            Direction::South => Direction::North,
+            Direction::SouthWest => Direction::NorthEast,
+            Direction::West => Direction::East,
+        }
+    }
+}
+impl Display for Direction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
     }
 }
 
@@ -79,25 +100,69 @@ pub fn part_one(input: &str) -> Option<u32> {
             }
         }
     }
-    dbg!(&count);
     Some(count)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
     let grid = make_grid(input);
     let directions = make_directions();
-    let mut count = 0;
-    for (y, line) in grid.iter().enumerate() {
-        for (x, c) in line.iter().enumerate() {
-            if c == &'A' {
-                let neighbors = directions
-                    .iter()
-                    .map(|d| offset_coord((x as isize, y as isize), d, 1))
-                    .collect::<Vec<(isize, isize)>>();
-                dbg!(&neighbors);
+    let a_coords = grid
+        .iter()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.iter().enumerate().filter_map(
+                move |(x, c)| {
+                    if c == &'A' {
+                        Some((x, y, c))
+                    } else {
+                        None
+                    }
+                },
+            )
+        })
+        .collect_vec();
+    let mut total_mas_count = 0;
+    'a_coords: for (x, y, _) in a_coords {
+        // Get all neighboring coords to each 'A'
+        let neighbors = directions
+            .iter()
+            .filter_map(|d| {
+                let (nx, ny) = offset_coord((x as isize, y as isize), d, 1);
+                if nx >= 0 && ny >= 0 && nx < grid[0].len() as isize && ny < grid.len() as isize {
+                    let nc = grid[ny as usize][nx as usize];
+                    Some((nx, ny, nc, d))
+                } else {
+                    None
+                }
+            })
+            .collect_vec();
+        let m_count = neighbors.iter().filter(|(_, _, c, _)| c == &'M').count();
+        let s_count = neighbors.iter().filter(|(_, _, c, _)| c == &'S').count();
+        if m_count >= 2 && s_count >= 2 {
+            let mut neighbor_mas_count = 0;
+            for (mx, my, c, d) in neighbors {
+                if c == 'M'
+                    && (d == &Direction::NorthEast
+                        || d == &Direction::SouthWest
+                        || d == &Direction::NorthWest
+                        || d == &Direction::SouthEast)
+                {
+                    let (sx, sy) = offset_coord((mx, my), &d.opposite(), 2);
+                    if sx >= 0 && sy >= 0 && sx < grid[0].len() as isize && sy < grid.len() as isize
+                    {
+                        if grid[sy as usize][sx as usize] == 'S' {
+                            neighbor_mas_count += 1;
+                            if neighbor_mas_count >= 2 {
+                                total_mas_count += 1;
+                                continue 'a_coords;
+                            }
+                        }
+                    }
+                }
             }
         }
     }
+    Some(total_mas_count)
 }
 
 #[cfg(test)]
@@ -114,7 +179,9 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(9));
     }
 }
